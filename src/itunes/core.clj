09b40e66ff38883667e04fbeaae85e36de2aa1cd
Vsevolod Romashov <7@7vn.ru>
2020-04-19 18:31:12 +0300

    add itunes.core/transform-track

--- src/itunes/core.clj
+++ src/itunes/core.clj
@@ -1,6 +1,8 @@
 (ns itunes.core
-  (:require [clojure.data.xml :refer [parse-str]]
-            [clojure.java.shell :refer [sh]]))
+  (:require [camel-snake-kebab.core :refer [->kebab-case-keyword]]
+            [clojure.data.xml :refer [parse-str]]
+            [clojure.java.shell :refer [sh]])
+  (:import [java.text SimpleDateFormat]))
 
 (def path
   (let [relative-path "~/Music/iTunes/iTunes Music Library.xml"]
@@ -8,6 +10,24 @@
 
 (def data
   (-> path slurp parse-str))
+
+(defn- transform-value [{:keys [tag content]}]
+  (let [date-format (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ssX")]
+    (case tag
+      :integer (Long/parseLong (first content))
+      :string (first content)
+      :date (.parse date-format (first content))
+      :true true)))
+
+(defn- transform-track [track]
+  (->> track
+       :content
+       (partition 2)
+       (reduce (fn [m [k v]]
+                 (assoc m
+                        (-> k :content first ->kebab-case-keyword)
+                        (transform-value v)))
+               {})))
 
 (def tracks
   (->> data
@@ -17,4 +37,5 @@
        (drop-while #(not= (:content %) '("Tracks")))
        second
        :content
-       (remove #(= (:tag %) :key))))
+       (remove #(= (:tag %) :key))
+       (map transform-track)))
